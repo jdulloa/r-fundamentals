c9c227d4e7aba1d8d56390d3a89b749295baa2ab
Jose <jduvmx@gmail.com>
2025-03-15 19:45:11 -0600

    Initial commit

---
+++ Fundamentals.R
@@ -0,0 +1,15 @@
+# Object
+Calculation = 5 + 5
+# Calling object
+Calculation
+# Vector
+KPIExample = c(1,2,3)
+# Installing a package
+install.packages("gcookbook")
+library(gcookbook)
+# Show data
+data()
+# Object
+baseball = tophitters2001
+# Summary function
+summary(baseball)
